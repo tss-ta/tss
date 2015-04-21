1bd1cfffee2c7fc86ee31145c3b900db94b6da4a
maks0 <maksbrunarskiy@gmail.com>
2015-04-21 20:05:54 +0300

    fixed pom.xml

--- tss-web/src/main/java/com/netcracker/tss/web/servlet/AdminHomeServlet.java
+++ tss-web/src/main/java/com/netcracker/tss/web/servlet/AdminHomeServlet.java
@@ -10,29 +10,27 @@ import java.io.IOException;
 /**
  * Created by Kyrylo Berehovyi on 20/04/2015.
  */
-
 @WebServlet(urlPatterns = "/admin")
 public class AdminHomeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setAttribute("title", "Admin Page");
-                req.setAttribute("title", "Admin Page");
+        req.setAttribute("title", "Admin Page");
         String menu = req.getParameter("menu");
 //        switch (menu){
 //            case "car": car
 //        }
 
-        
-        if ("cars".equals(menu)){
+        if ("cars".equals(menu)) {
             req.getRequestDispatcher("/WEB-INF/views/admin/cars.jsp").forward(req, resp);
-        } else if ("reports".equals(menu)){
+        } else if ("reports".equals(menu)) {
             req.getRequestDispatcher("/WEB-INF/views/admin/reports.jsp").forward(req, resp);
-        } else if ("customers".equals(menu)){
-             req.getRequestDispatcher("/WEB-INF/views/admin/reports.jsp").forward(req, resp);
+        } else if ("customers".equals(menu)) {
+            req.getRequestDispatcher("/WEB-INF/views/admin/customers.jsp").forward(req, resp);
         } else {
 
-        req.getRequestDispatcher("/WEB-INF/views/admin/cars.jsp").forward(req, resp);
+            req.getRequestDispatcher("/WEB-INF/views/admin/cars.jsp").forward(req, resp);
         }
     }
 }
