d3cba7df2097c70fd39b410feddb6ba294be98d2
Stanislav <szabielin@gmail.com>
2015-04-27 22:27:10 +0300

    Added UI for Guest Order.
    Also fixed minor errors in TaxiOrderBean

--- tss-ejb/src/main/java/com/netcracker/DTO/GroupDTO.java
+++ tss-ejb/src/main/java/com/netcracker/DTO/GroupDTO.java
@@ -14,7 +14,7 @@ import java.util.List;
  * @author maks
  */
 public class GroupDTO implements Serializable {
-
+ 
     private int id;
 
     private String name;
--- tss-ejb/src/main/java/com/netcracker/ejb/TaxiOrderBean.java
+++ tss-ejb/src/main/java/com/netcracker/ejb/TaxiOrderBean.java
@@ -13,11 +13,12 @@ import com.netcracker.entity.Address;
 import com.netcracker.entity.Route;
 import com.netcracker.entity.TaxiOrder;
 import com.netcracker.entity.User;
+
 import java.rmi.RemoteException;
+
 import javax.ejb.EJBException;
 import javax.ejb.SessionBean;
 import javax.ejb.SessionContext;
-
 
 /**
  *
@@ -25,66 +26,63 @@ import javax.ejb.SessionContext;
  */
 public class TaxiOrderBean implements SessionBean {
 
-    public void addTaxiOrder(User user, Route route, Address addFrom, Address addTo, TaxiOrder taxiOrder) {
-        AddressDAO addressDAO = null;
-        RouteDAO routeDAO = null;
-        TaxiOrderDAO taxiOrderDAO = null;
-        UserDAO userDAO = null;
-        try {
-            addressDAO = new AddressDAO();
-            addressDAO.persist(addFrom);
-            addressDAO.persist(addTo);
-            routeDAO = new RouteDAO();
-            route.setFromAddrId(addFrom);
-            route.setToAddrId(addTo);
-            routeDAO.persist(route);
-            taxiOrder.setRouteId(route);
-            userDAO = new UserDAO();
-            User userFromDB = userDAO.getByEmail(user.getEmail());
-            if (userFromDB != null) {
-                taxiOrder.setUserId(userFromDB);
-            } else {
-                userDAO.persist(user);
-                taxiOrder.setUserId(user);
-            }
-            taxiOrder.setRouteId(route);
-            taxiOrderDAO = new TaxiOrderDAO();
-            taxiOrderDAO.persist(taxiOrder);
-        } finally {
-            if (addressDAO != null) {
-                addressDAO.close();
-            }
-            if (routeDAO != null) {
-                routeDAO.close();
-            }
-            if (userDAO != null) {
-                userDAO.close();
-            }
-            if (taxiOrderDAO != null) {
-                taxiOrderDAO.close();
-            }
-        }
+	public void addTaxiOrder(User user, Route route, Address addFrom,
+			Address addTo, TaxiOrder taxiOrder, boolean isGuest) {
+		AddressDAO addressDAO = null;
+		RouteDAO routeDAO = null;
+		TaxiOrderDAO taxiOrderDAO = null;
+		UserDAO userDAO = null;
+		try {
+			addressDAO = new AddressDAO();
+			addressDAO.persist(addFrom);
+			addressDAO.persist(addTo);
+			routeDAO = new RouteDAO();
+			route.setFromAddrId(addFrom);
+			route.setToAddrId(addTo);
+			routeDAO.persist(route);
+			taxiOrder.setRouteId(route);
+			userDAO = new UserDAO();
+			User userFromDB = userDAO.getByEmail(user.getEmail());
+			taxiOrder.setUserId(userFromDB);
+			taxiOrder.setRouteId(route);
+			taxiOrderDAO = new TaxiOrderDAO();
+			taxiOrderDAO.persist(taxiOrder);
+		} finally {
+			if (addressDAO != null) {
+				addressDAO.close();
+			}
+			if (routeDAO != null) {
+				routeDAO.close();
+			}
+			if (userDAO != null) {
+				userDAO.close();
+			}
+			if (taxiOrderDAO != null) {
+				taxiOrderDAO.close();
+			}
+		}
 
-    }
+	}
 
-    @Override
-    public void setSessionContext(SessionContext ctx) throws EJBException, RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	@Override
+	public void setSessionContext(SessionContext ctx) throws EJBException,
+			RemoteException {
 
-    @Override
-    public void ejbRemove() throws EJBException, RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	}
 
-    @Override
-    public void ejbActivate() throws EJBException, RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	@Override
+	public void ejbRemove() throws EJBException, RemoteException {
 
-    @Override
-    public void ejbPassivate() throws EJBException, RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	}
+
+	@Override
+	public void ejbActivate() throws EJBException, RemoteException {
+
+	}
+
+	@Override
+	public void ejbPassivate() throws EJBException, RemoteException {
+
+	}
 
 }
