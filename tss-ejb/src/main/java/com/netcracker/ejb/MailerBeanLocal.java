4c400aea797d15357965eaf9dfd56a589c2e3fa4
Stanislav <szabielin@gmail.com>
2015-05-03 14:27:35 +0300

    Basic Mailer Function Added

---
+++ tss-ejb/src/main/java/com/netcracker/ejb/MailerBeanLocal.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.netcracker.ejb;
+
+import java.io.IOException;
+
+import javax.ejb.EJBLocalObject;
+
+import org.json.JSONException;
+
+import com.netcracker.entity.User;
+
+/**
+ *
+ * @author Stanislav Zabielin
+ */
+public interface MailerBeanLocal extends EJBLocalObject {
+
+	public void sendEmail(User user, String title, String msg);
+
+}
