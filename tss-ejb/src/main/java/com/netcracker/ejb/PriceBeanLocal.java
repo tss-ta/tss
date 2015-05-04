7ce27d3ad14b5ebfee4d59c1eb3eb39378ab999f
Viktor Shaban <blacksmith771@gmail.com>
2015-05-05 00:12:39 +0300

    Added PriceBean

---
+++ tss-ejb/src/main/java/com/netcracker/ejb/PriceBeanLocal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.netcracker.ejb;
+
+import java.util.Date;
+import javax.ejb.EJBLocalObject;
+
+/**
+ *
+ * @author Виктор
+ */
+public interface PriceBeanLocal extends EJBLocalObject {
+
+    public double calculatePrice(float distance, Date orderTime);
+
+}
