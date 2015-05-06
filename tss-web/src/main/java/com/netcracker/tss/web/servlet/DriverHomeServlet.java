693ebc81e8515143cc12784f3586841f09b7eaca
Vitalii Chekaliuk <lobzik2007@gmail.com>
2015-05-06 22:56:43 +0300

    Added templates for driver ui.

--- tss-web/src/main/java/com/netcracker/tss/web/servlet/DriverHomeServlet.java
+++ tss-web/src/main/java/com/netcracker/tss/web/servlet/DriverHomeServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * Created by Kyrylo Berehovyi on 20/04/2015.
+ * Created by Vitalii Chekaliuk on 06/05/2015.
  */
 
 @WebServlet(urlPatterns = "/driver")
@@ -17,7 +17,6 @@ public class DriverHomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setAttribute("title", "Driver Page");
-
-        req.getRequestDispatcher("/WEB-INF/views/driver/home-driver.jsp").forward(req, resp);
+        resp.sendRedirect("/driver/dashboard");
     }
 }
