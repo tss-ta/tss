cf427754c7db6791813cb5da7d549227bc775676
bbuallbest <bbuallbest@gmail.com>
2015-05-17 22:45:21 +0300

    Tariff name was rolled back

--- scripts/fill_user_tables.sql
+++ scripts/fill_user_tables.sql
@@ -276,103 +276,71 @@ INSERT INTO contacts
 	VALUES
 	('Monica', 'monica.harmonica@gmail.com', 10);
 
--- INSERT INTO tariff
--- (tariff_name, plus_coef, multiple_coef)
--- VALUES
--- ('van', 20, 1.1);
--- INSERT INTO tariff
--- (tariff_name, plus_coef, multiple_coef)
--- VALUES
--- ('cargo', 20, 1.5);
--- INSERT INTO tariff
--- (tariff_name, plus_coef, multiple_coef)
--- VALUES
--- ('business', 30, 1.4);
--- INSERT INTO tariff
--- (tariff_name, plus_coef, multiple_coef)
--- VALUES
--- ('economy', 0, 0.9);
--- INSERT INTO tariff
--- (tariff_name, plus_coef, multiple_coef)
--- VALUES
--- ('van', 20, 1.1);
--- INSERT INTO tariff
--- (tariff_name, plus_coef, multiple_coef)
--- VALUES
--- ('wi-fi', 10, 1.01);
--- INSERT INTO tariff
--- (tariff_name, plus_coef, multiple_coef)
--- VALUES
--- ('conditioner', 10, 1.07);
--- INSERT INTO tariff
--- (tariff_name, plus_coef, multiple_coef)
--- VALUES
--- ('no_smoked', 10, 1.01);
--- INSERT INTO tariff
--- (tariff_name, plus_coef, multiple_coef)
--- VALUES
--- ('missed', 50, 1);
--- INSERT INTO tariff
--- (tariff_name, plus_coef, multiple_coef)
--- VALUES
--- ('weekend', 10, 1.03);
--- INSERT INTO tariff
--- (tariff_name, plus_coef, multiple_coef)
--- VALUES
--- ('night', 40, 1.07);
--- INSERT INTO tariff
--- (tariff_name, plus_coef, multiple_coef)
--- VALUES
--- ('rush_hour', 10, 1.3);
--- INSERT INTO tariff
--- (tariff_name, plus_coef, multiple_coef)
--- VALUES
--- ('celebration', 100, 1.3);
--- INSERT INTO tariff
--- (tariff_name, plus_coef, multiple_coef)
--- VALUES
--- ('meet_guest', 100, 1.03);
--- INSERT INTO tariff
--- (tariff_name, plus_coef, multiple_coef)
--- VALUES
--- ('convey', 0, 0.9);
--- INSERT INTO tariff
--- (tariff_name, plus_coef, multiple_coef)
--- VALUES
--- ('sober', 0, 0.9);
--- INSERT INTO tariff
--- (tariff_name, plus_coef, multiple_coef)
--- VALUES
--- ('animal', 20, 1.1);
--- INSERT INTO tariff
--- (tariff_name, plus_coef, multiple_coef)
--- VALUES
--- ('per_km', 5, 1);
--- INSERT INTO tariff
--- (tariff_name, plus_coef, multiple_coef)
--- VALUES
--- ('per_car', 100, 1.2);
--- INSERT INTO tariff
--- (tariff_name, plus_coef, multiple_coef)
--- VALUES
--- ('per_hour', 20, 1.1);
+INSERT INTO tariff
+(tariff_name, plus_coef, multiple_coef)
+VALUES
+('van', 20, 1.1);
+INSERT INTO tariff
+(tariff_name, plus_coef, multiple_coef)
+VALUES
+('cargo', 20, 1.5);
+INSERT INTO tariff
+(tariff_name, plus_coef, multiple_coef)
+VALUES
+('business', 30, 1.4);
+INSERT INTO tariff
+(tariff_name, plus_coef, multiple_coef)
+VALUES
+('economy', 0, 0.9);
+INSERT INTO tariff
+(tariff_name, plus_coef, multiple_coef)
+VALUES
+('wi-fi', 10, 1.01);
+INSERT INTO tariff
+(tariff_name, plus_coef, multiple_coef)
+VALUES
+('conditioner', 10, 1.07);
+INSERT INTO tariff
+(tariff_name, plus_coef, multiple_coef)
+VALUES
+('no_smoked', 10, 1.01);
+INSERT INTO tariff
+(tariff_name, plus_coef, multiple_coef)
+VALUES
+('missed', 50, 1);
+INSERT INTO tariff
+(tariff_name, plus_coef, multiple_coef)
+VALUES
+('weekend', 10, 1.03);
+INSERT INTO tariff
+(tariff_name, plus_coef, multiple_coef)
+VALUES
+('night', 40, 1.07);
+INSERT INTO tariff
+(tariff_name, plus_coef, multiple_coef)
+VALUES
+('rush_hour', 10, 1.3);
+INSERT INTO tariff
+(tariff_name, plus_coef, multiple_coef)
+VALUES
+('celebration', 100, 1.3);
+INSERT INTO tariff
+(tariff_name, plus_coef, multiple_coef)
+VALUES
+('meet_guest', 100, 1.03);
+INSERT INTO tariff
+(tariff_name, plus_coef, multiple_coef)
+VALUES
+('convey', 0, 0.9);
+INSERT INTO tariff
+(tariff_name, plus_coef, multiple_coef)
+VALUES
+  ('animal', 20, 1.1);
+INSERT INTO tariff
+(tariff_name, plus_coef, multiple_coef)
+VALUES
+  ('per_km', 5, 1);
 
-INSERT INTO tariff (tariff_name, plus_coef, multiple_coef) VALUES ('van car', 20, 1.1);
-INSERT INTO tariff (tariff_name, plus_coef, multiple_coef) VALUES ('cargo car', 20, 1.5);
-INSERT INTO tariff (tariff_name, plus_coef, multiple_coef) VALUES ('business car', 30, 1.4);
-INSERT INTO tariff (tariff_name, plus_coef, multiple_coef) VALUES ('economy car', 0, 0.9);
-INSERT INTO tariff (tariff_name, plus_coef, multiple_coef) VALUES ('wi-fi', 10, 1.01);
-INSERT INTO tariff (tariff_name, plus_coef, multiple_coef) VALUES ('conditioner', 10, 1.07);
-INSERT INTO tariff (tariff_name, plus_coef, multiple_coef) VALUES ('no smoke driver', 10, 1.01);
-INSERT INTO tariff (tariff_name, plus_coef, multiple_coef) VALUES ('missed', 50, 1);
-INSERT INTO tariff (tariff_name, plus_coef, multiple_coef) VALUES ('weekend', 10, 1.03);
-INSERT INTO tariff (tariff_name, plus_coef, multiple_coef) VALUES ('night', 40, 1.07);
-INSERT INTO tariff (tariff_name, plus_coef, multiple_coef) VALUES ('rush hour', 10, 1.3);
-INSERT INTO tariff (tariff_name, plus_coef, multiple_coef) VALUES ('celebration', 100, 1.3);
-INSERT INTO tariff (tariff_name, plus_coef, multiple_coef) VALUES ('meet my guest', 100, 1.03);
-INSERT INTO tariff (tariff_name, plus_coef, multiple_coef) VALUES ('convey corporation', 0, 0.9);
-INSERT INTO tariff (tariff_name, plus_coef, multiple_coef) VALUES ('animal transportation', 20, 1.1);
-INSERT INTO tariff (tariff_name, plus_coef, multiple_coef) VALUES ('per/km', 5, 1);
 
 INSERT INTO driver_car (id, driver_id, car_id, assign_time, unassign_time) VALUES (DEFAULT, 19, 1, null, null);
 INSERT INTO driver_car (id, driver_id, car_id, assign_time, unassign_time) VALUES (DEFAULT, 12, 2, null, null);
